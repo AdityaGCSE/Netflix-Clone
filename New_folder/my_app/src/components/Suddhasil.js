5b1f54d43bad2157e69453bae89438bcbc13a2e5
AdityaGCSE <adityag30802@gmail.com>
2023-01-14 19:31:30 +0530

    initial commit

--- New_folder/my_app/src/components/Suddhasil.js
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-    return (
-        <div className="container">
-            <div> <br /> <h3>Welcome to Suddhasil Advanced GPL</h3> <br /> </div>
-        </div>
-    )
-}
